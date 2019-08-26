cb53bf45cd83534d782e56bf829160349de8b4a6
Yan_Moraes <yan_moraes@estudante.sc.senai.br>
2019-08-26 12:30:57 -0300

    Adicionado partes do CSS

--- Conversor.js
+++ Conversor.js
@@ -7,18 +7,18 @@ var fObj = document.convert.ftemp, cObj = document.convert.ctemp, kObj = documen
 if (direction == "ftoc") 
 {
     cObj.value = Math.round((fObj.value - 32) * (5/9));
-	kObj.value = Math.round((parseInt(cObj.value) + 459.67) * (5/9));
+	kObj.value = Math.round((parseFloat(cObj.value) + 459.67) * (5/9));
 } 
 else if (direction == "ktof") 
  {
-	fObj.value = Math.round((parseInt(cObj.value) * (9/5)) -  459.67);
+	fObj.value = Math.round((parseFloat(cObj.value) * (9/5)) -  459.67);
 	cObj.value = Math.round((fObj.value - 32) * (5/9));
 	
  }
 else
  {
-	fObj.value = Math.round((parseInt(cObj.value) * (9/5)) + 32);
-	kObj.value = Math.round((parseInt(cObj.value) + 273));
+	fObj.value = Math.round((parseFloat(cObj.value) * (9/5)) + 32);
+	kObj.value = Math.round((parseFloat(cObj.value) + 273));
  }
 }
 
