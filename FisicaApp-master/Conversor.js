cba2c2684ce29bef90e14cdd239a3f459a5f7e25
Yan <48677145+yanmoraes32@users.noreply.github.com>
2019-09-05 09:17:13 -0300

    atualização 4

--- FisicaApp-master/Conversor.js
+++ FisicaApp-master/Conversor.js
@@ -42,3 +42,34 @@ function fonte(e){
 		elemento.css("fontSize", parseInt(fonte) - 1);
 	}
 }
+function date_time(id){
+
+
+    date = new Date;
+    year = date.getFullYear();
+    month = date.getMonth();
+    months = new Array('Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outbro', 'Novembro', 'Dezembro');
+    d = date.getDate();
+    day = date.getDay();
+    days = new Array('Domingo', 'Segunda - Feira', 'Terça - Feira', 'Quarta - Feira', 'Quinta - Feira', 'Sexta - Feira', 'Sabado');
+    h = date.getHours();
+    if(h<10)
+    {
+            h = "0"+h;
+    }
+    m = date.getMinutes();
+    if(m<10)
+    {
+            m = "0"+m;
+    }
+    s = date.getSeconds();
+    if(s<10)
+    {
+            s = "0"+s;
+    }
+    result = 'Bem Vindo, Hoje é '+days[day]+' '+d+' '+months[month]+' '+year + " . Aproveite nosso App";
+    document.getElementById(id).innerHTML = result;
+    setTimeout('date_time("'+id+'");','1000');
+    return true;
+	}
+	
